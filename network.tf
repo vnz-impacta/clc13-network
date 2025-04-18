1e81f2f75cfa6bf49a7dd9f8e7099a17c9842507
EC2 Default User <ec2-user@ip-172-31-85-29.ec2.internal>
2025-04-18 19:24:32 +0000

    Update: Correção de Vulnerabilidades do Checkov

--- network.tf
+++ network.tf
@@ -169,3 +169,24 @@ resource "aws_nat_gateway" "nat_gw_1b" {
   # on the Internet Gateway for the VPC.
   depends_on = [aws_internet_gateway.igw]
 }
+
+
+
+#### CORRECAO DAS VULNERABILIDADES DO CHECKOV #####
+
+# Correcao primeira issue
+resource "aws_flow_log" "flow_log" {
+  log_destination      = "arn:aws:s3:::zorza-clc13-network-terraform-state"
+  log_destination_type = "s3"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.minha_vpc.id
+}
+
+# Correcao segunda issue
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.minha_vpc.id
+  
+  tags = {
+    Name = "my-iac-sg"
+  }
+}
